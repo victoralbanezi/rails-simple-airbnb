62319be9778dbf2105ea21016bfba80b7fc62379
victor albanezi <victor.albanezi@hotmail.com>
2020-04-24 00:21:17 -0300

    .

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,48 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flats_path
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to flats_path
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: "Light & Spacious Garden Flat London",
+  address: "10 Clifton Gardens London W9 1DT",
+  description: "A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory",
+  price_per_night: 75,
+  number_of_guests: 3,
+)
+Flat.create!(
+  name: "Flat in Ny",
+  address: "5th Avenue",
+  description: "Bed sincerity yet therefore forfeited his certainty neglected questions. Pursuit ",
+  price_per_night: 150,
+  number_of_guests: 6,
+)
+Flat.create!(
+  name: "Penthouse in Chigago",
+  address: "Random street",
+  description: "wenty limits as months. Inhabit so perhaps of in to certain. Sex excuse chatty was seemed warmth. Nay add far few ",
+  price_per_night: 300,
+  number_of_guests: 9,
+)
+Flat.create!(
+  name: "Lovely apartment near the beach",
+  address: "10 Str, N 412",
+  description: "Consisted we otherwise arranging commanded discovery it explained. ",
+  price_per_night: 600,
+  number_of_guests: 12,
+)
+Flat.create!(
+  name: "Lovely flat in the mountains",
+  address: "5, Str 1, Nu 23",
+  description: "Inhabit so perhaps of in to certain. Sex excuse . ",
+  price_per_night: 325,
+  number_of_guests: 12,
+)
